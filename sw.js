886af9b557af024644dcd5373c1d87764fe21fc8
古松 <gusong@badambiz.com>
2025-02-08 15:33:23 +0800

    deploy

---
+++ sw.js
@@ -0,0 +1,154 @@
+
+
+const cacheVersion = 'v1.0.333'
+
+console.log('service worker version', '1.0.333')
+
+const routes = ['home','pay','test','login','fines','feedback','feedback-success','']
+
+const public_path = '/'
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(cacheVersion)
+  await cache.put(request, response)
+}
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(cacheVersion)
+  await cache.addAll(resources)
+}
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable()
+  }
+}
+
+function clearOldCache() {
+  return caches.keys().then(function (keyList) {
+    return Promise.all(
+      keyList.map(function (key) {
+        if (key !== cacheVersion) {
+          return caches.delete(key)
+        }
+      })
+    )
+  })
+}
+
+self.addEventListener('activate', (event) => {
+  console.log('service worker active', '1.0.333', event)
+  event.waitUntil(clearOldCache())
+  event.waitUntil(clients.claim())
+})
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(addResourcesToCache(['home/index.html','pay/index.html','test/index.html','login/index.html','fines/index.html','feedback/index.html','feedback-success/index.html','/index.html']))
+})
+
+const strategies = {
+  networkFirst: 1,
+  cacheFirst: 2,
+}
+
+const cacheFirst = async (event) => {
+  let request = event.request
+  const responseFromCache = await caches.match(request)
+  if (responseFromCache) {
+    return responseFromCache
+  }
+  try {
+    const responseFromNetwork = await fetch(request.clone())
+    putInCache(request, responseFromNetwork.clone())
+    return responseFromNetwork
+  } catch (error) {
+    return errorRes()
+  }
+}
+
+const networkFirst = async (event) => {
+  let request = event.request
+  try {
+    const responseFromNetwork = await fetch(request.clone())
+    putInCache(request, responseFromNetwork.clone())
+    return responseFromNetwork
+  } catch (error) {
+    postMessage(event, { event: 'offline' })
+    const url = request.url.split('?')[0]
+    const r = matchRoute(url)
+    if (r) {
+      const index = getIndexUrl(url)
+      request = index
+    }
+    const responseFromCache = await caches.match(request)
+    if (responseFromCache) {
+      return responseFromCache
+    }
+    return errorRes()
+  }
+}
+
+function postMessage(event, data) {
+  try {
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+          let msg = JSON.stringify({
+            client: client.id,
+            data: data,
+          })
+          client.postMessage(msg)
+      })
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+function errorRes() {
+  return new Response('Network error happened', {
+    status: 408,
+    headers: { 'Content-Type': 'text/plain' },
+  })
+}
+
+function getResourceByStrategy(request, first) {
+  if (first === strategies.cacheFirst) {
+    return cacheFirst(request)
+  }
+  return networkFirst(request)
+}
+
+function matchRoute(url) {
+  for (let i = 0; i < routes.length; i += 1) {
+    const r1 = location.origin + public_path + routes[i]
+    const r2 = location.origin + public_path + routes[i] + '/'
+    if (url === r1 || url === r2) {
+      return true
+    }
+  }
+  return false
+}
+
+function getIndexUrl(url) {
+  return url.endsWith('/') ? url + 'index.html' : url + '/index.html'
+}
+
+self.addEventListener('fetch', function (event) {
+  try {
+    if (event.request.url.indexOf(location.origin) >= 0) {
+      const origin_url = event.request.url.split('?')[0]
+      if (event.request.url.endsWith('.html') || matchRoute(origin_url) || event.request.url.indexOf('assets') < 0 || event.request.url.indexOf('sawa_logo.ico') >= 0) {
+        console.log('service worker', '1.0.333')
+        event.respondWith(networkFirst(event))
+      } else {
+        event.respondWith(cacheFirst(event))
+      }
+    }
+  } catch (e) {
+    event.respondWith(networkFirst(event))
+  }
+})
+
+
+
